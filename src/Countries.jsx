05ef286939da8b391d7b2839469d40086064c2a5
code-with-zulquar <codewithzulquar@gmail.com>
2024-10-01 16:44:16 +0530

    update

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -3,17 +3,53 @@ import Country from "./Country";
 
 export default function Countries() {
   const [countries, setcountries] = useState([]);
+  const [visitedCountries, setvisitedCountries] = useState([]);
+  const [visitedflag, setVisitedflag] = useState([]);
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
       .then((data) => setcountries(data));
   }, []);
+
+  const handleVisitedCountries = (country) => {
+    console.log("Add this to your visited Countries");
+
+    const newVisitedCountries = [...visitedCountries, country];
+    setvisitedCountries(newVisitedCountries);
+  };
+  const handleVisitedflag = (flag) => {
+    const newVisitedflag = [...visitedflag, flag];
+    setVisitedflag(newVisitedflag);
+  };
+
   return (
-    <div className="flex justify-center flex-wrap gap-3 mt-2">
-      {countries.map((country, index) => (
-        <Country country={country} key={index} />
-      ))}
+    <div>
+      <div className=" flex justify-center m-10">
+        <h1 className=" text-xl font-bold fixed">
+          Visited Countries : {visitedCountries.length}
+        </h1>
+        <ul className="mt-10">
+          {visitedCountries.map((country) => (
+            <li>{country.name.common}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="flex gap-1 justify-center mb-2">
+        {visitedflag.map((flag) => (
+          <img src={flag} className="w-10 h-10"></img>
+        ))}
+      </div>
+      <div className="flex justify-center flex-wrap gap-3 ">
+        {countries.map((country, index) => (
+          <Country
+            country={country}
+            key={index}
+            handleVisitedCountries={handleVisitedCountries}
+            handleVisitedflag={handleVisitedflag}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/Country.jsx
+++ src/Country.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 
-export default function Country({ country }) {
+export default function Country({
+  country,
+  handleVisitedCountries,
+  handleVisitedflag,
+}) {
   const style = " w-72 h-4/5 rounded-xl border-2 border-black";
   const imgStyle = " w-full h-44 rounded-xl p-1";
   const textStyle = "text-xl font-bold text-center m-1";
@@ -28,6 +32,19 @@ export default function Country({ country }) {
       <h1 className={textStyle}>
         {visited ? "I visited this Country" : "I want to Visit this country"}
       </h1>
+      <button
+        onClick={() => handleVisitedCountries(country)}
+        className="border-2 border-black px-2 py-1 rounded m-auto block mt-3 font-bold"
+      >
+        {" "}
+        click me
+      </button>
+      <button
+        onClick={() => handleVisitedflag(country.flags.png)}
+        className="border-2 border-black px-2 py-1 rounded m-auto block mt-3 font-bold"
+      >
+        click me for flag
+      </button>
     </div>
   );
 }
